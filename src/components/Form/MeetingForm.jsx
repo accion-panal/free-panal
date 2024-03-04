bfd6eacac6db89f181ce96e372cb187725697a73
Fabian Salas <fabian.salas.astete@gmail.com>
2024-03-04 11:20:33 -0300

    fixed: fixed responsive

--- src/components/Form/MeetingForm.jsx
+++ src/components/Form/MeetingForm.jsx
@@ -26,17 +26,6 @@ const MeetingForm = ({ title, subtitle, }) => {
     meetingDate: 'No indicada',
   });
 
-  // Variables Date para poder concatenar con MeetingDate
-  // const [meetingDate, setMeetingDate] = useState(new Date());
-  // const [meetingTime, setMeetingTime] = useState('');
-
-  // const handleDateChange = (event) => {
-  //   setMeetingDate(event.target.value);
-  // };
-
-  // const handleTimeChange = (event) => {
-  //   setMeetingTime(event.target.value);
-  // };
 
   const [errorMsg, setErrorMsg] = useState({
     allFieldRequierd: '',
@@ -82,19 +71,6 @@ const MeetingForm = ({ title, subtitle, }) => {
     });
   };
 
-  // const handleAction = (ev) => {
-  //   setFormData({
-  //     ...formData,
-  //     action: ev.target.value,
-  //   });
-  // };
-
-  // const handleTermsAndConditions = (ev) => {
-  //   setFormData({
-  //     ...formData,
-  //     termsAndConditions: !formData.termsAndConditions,
-  //   });
-  // };
 
   const resetForm = () => {
     setFormData({
@@ -163,21 +139,6 @@ const MeetingForm = ({ title, subtitle, }) => {
         return;
       }
 
-      // if (!meetingDate || !meetingTime) {
-      //   setErrorMsg({
-      //     allFieldRequierd:
-      //       'Por favor debes definir una fecha y hora de encuentro',
-      //   });
-      //   return;
-      // }
-
-      // if (!formData.meetingDate) {
-      //   setErrorMsg({
-      //     allFieldRequierd:
-      //       'Por favor debes definir una fecha y hora de encuentro',
-      //   });
-      //   return;
-      // }
 
       setLoading(true);
       const response = await ContactFormServices.sendContactMeetingForm(
@@ -239,7 +200,7 @@ const MeetingForm = ({ title, subtitle, }) => {
     <div className="rounded-[10px] p-4 my-10 xl:py-5 xl:px-2 xl:m-0 w-full xl:w-[500px] shadow-xl">
       {/* xl:w-3/5 */}
         <div className="text-center  rounded-full  font-semibold mb-2">
-          <h2 className="text-5xl xl:text-4xl font-semibold py-3 text-primary uppercase">{title}</h2>
+          <h2 className="text-3xl xl:text-4xl font-semibold py-3 text-primary uppercase">{title}</h2>
         </div>
         {/* <div className="text-start py-1 flex items-center">
           <MdPhoneIphone className='w-[30px] h-[30px] border border-2 rounded-full p-1 mx-1'/><span className='font-normal '><b>Teléfono: </b> +56 9 321 421 76 </span>
@@ -297,26 +258,6 @@ const MeetingForm = ({ title, subtitle, }) => {
             ></textarea>
           </div>
         </div>
-
-        {/* <div className="w-5/6 mx-auto my-14 mb-10 flex items-center justify-center">
-          <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
-            <input
-              className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#ca6f3b] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary"
-              type="checkbox"
-              id="terms"
-              name="terms"
-              checked={formData?.termsAndConditions}
-              onChange={handleTermsAndConditions}
-            />
-            <label
-              className="inline-block pl-[0.15rem] hover:cursor-pointer text-gray-600"
-              htmlFor="terms"
-            >
-              Al continuar estás aceptando los términos y condiciones y la
-              política de privacidad
-            </label>
-          </div>
-        </div> */}
 
         {errorMsg?.serverEmailError && (
           <Alert type="warning" message={errorMsg?.serverEmailError} />
